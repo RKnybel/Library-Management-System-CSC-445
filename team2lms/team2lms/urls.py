b33f1140cb3e2d45ab468f666f66a3e3043e20f2
Riley Knybel <spsknybeln@gmail.com>
2023-04-24 16:25:41 -0400

    Skeleton complete

--- team2lms/team2lms/urls.py
+++ team2lms/team2lms/urls.py
@@ -20,3 +20,16 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# Use include() to add paths from the catalog application
+from django.urls import include
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+]
