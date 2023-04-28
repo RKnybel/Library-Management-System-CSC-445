f343e7220c95ba3555ad4b175a8a767a23378783
Lauren Cappiello <103142031+Lcapp22928@users.noreply.github.com>
2023-04-28 18:22:52 -0400

    Additional Language Error Fix. This has working admin and basic html template front end functioning

--- team2lms/catalog/admin.py
+++ team2lms/catalog/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Author, Genre, Book, BookInstance, Language
+from .models import Author, Genre, Book, BookInstance
 
 admin.site.register(Book)
 admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(BookInstance)
-admin.site.register(Language)
