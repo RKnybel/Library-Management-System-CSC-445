0e48a2d903b4e2db66bef810afd7f34b847286c4
Riley Knybel <spsknybeln@gmail.com>
2023-04-27 20:22:08 -0400

    Update with code from blog

--- team2lms/catalog/models.py
+++ team2lms/catalog/models.py
@@ -2,60 +2,122 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
+from django.urls import reverse  # To generate URLS by reversing URL patterns
 
-    #fields
-    UserID = models.CharField(max_length = 6, primary_key = True, unique=True)
-    Password = models.CharField(max_length = 100)
-    UserFirstName = models.CharField(max_length = 20)
-    UserLastName = models.CharField(max_length = 20)
-    PhoneNumber = models.CharField(max_length = 6)
-    Email = models.EmailField()
-    AddressLine1 = models.CharField(max_length = 50)
-    AddressLine2 = models.CharField(max_length = 50)
-    City = models.CharField(max_length = 50)
-    State = models.CharField(max_length = 20)
-    ZipCode = models.CharField(max_length = 5)
-    UserTypeID = models.ForeignKey('UserType', on_delete=models.SET_DEFAULT, default=0)
-    NumBooksLost = models.IntegerField(max_length = 6, default=0)
-    TotalFeesDue = models.IntegerField(max_length = 6, default = 0)
 
-class UserType(models.Model):
-    UserTypeID = models.CharField(max_length = 5, primary_key=True, unique=True)
-    UserType = models.CharField(max_length=20)
+class Genre(models.Model):
+    """Model representing a book genre (e.g. Science Fiction, Non Fiction)."""
+    name = models.CharField(
+        max_length=200,
+        help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)"
+        )
 
-class Rental(models.Model):
-    UserID = models.ForeignKey('User', on_delete=models.SET_DEFAULT, primary_key=True, default=0)
-    ISBN = models.ForeignKey('Book', on_delete=models.SET_DEFAULT, primary_key=True, default=0)
-    DateRented = models.DateField()
-    RenewalDate = models.DateField()
-    FeeIfLate = models.IntegerField()
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
+        return self.name
+
+
+class Language(models.Model):
+    """Model representing a Language (e.g. English, French, Japanese, etc.)"""
+    name = models.CharField(max_length=200,
+                            help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
+        return self.name
+
 
 class Book(models.Model):
-    ISBN = models.IntegerField(max_length=13, primary_key=True, unique=True)
-    Title = models.CharField(max_length=100)
-    NumPages = models.BigIntegerField()
-    SeriesID = models.ForeignKey('Series', on_delete=models.SET_DEFAULT, default=0)
-    ReleaseDate = models.DateField()
-    BookType = models.ForeignKey('BookType', on_delete=models.SET_DEFAULT, default=0, max_length=20)
-    Genre = models.ForeignKey('BookGenre', on_delete=models.SET_DEFAULT, default=0)
+    """Model representing a book (but not a specific copy of a book)."""
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    # Foreign Key used because book can only have one author, but authors can have multiple books
+    # Author as a string rather than object because it hasn't been declared yet in file.
+    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
+    isbn = models.CharField('ISBN', max_length=13,
+                            unique=True,
+                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
+                                      '">ISBN number</a>')
+    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+    # ManyToManyField used because a genre can contain many books and a Book can cover many genres.
+    # Genre class has already been defined so we can specify the object above.
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+    
+    class Meta:
+        ordering = ['title', 'author']
 
-class BookAuthor(models.Model):
-    AuthorID = models.ForeignKey('Author', on_delete=models.SET_DEFAULT, primary_key=True, unique=True, default=0)
-    ISBN = models.ForeignKey('Book', on_delete=models.SET_DEFAULT, primary_key=True, default=0)
+    def display_genre(self):
+        """Creates a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+
+    display_genre.short_description = 'Genre'
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular book instance."""
+        return reverse('book-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.title
+
+
+import uuid  # Required for unique book instances
+from datetime import date
+
+from django.contrib.auth.models import User  # Required to assign User as a borrower
+
+
+class BookInstance(models.Model):
+    """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique ID for this particular book across whole library")
+    book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
+
+    LOAN_STATUS = (
+        ('d', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='d',
+        help_text='Book availability')
+
+    class Meta:
+        ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return '{0} ({1})'.format(self.id, self.book.title)
+
 
 class Author(models.Model):
-    AuthorID = models.CharField(max_length = 10, primary_key=True, unique=True)
-    AuthorFirstName = models.CharField(max_length=50)
-    AuthorLastName = models.CharField(max_length=50)
-    Alive = models.BooleanField()
+    """Model representing an author."""
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('died', null=True, blank=True)
 
-class Series(models.Model):
-    SeriesID = models.IntegerField(primary_key=True, unique=True)
-    SeriesName = models.CharField(max_length=50)
+    class Meta:
+        ordering = ['last_name', 'first_name']
 
-class BookType(models.Model):
-    BookType = models.CharField(primary_key=True, unique=True, max_length=20)
+    def get_absolute_url(self):
+        """Returns the url to access a particular author instance."""
+        return reverse('author-detail', args=[str(self.id)])
 
-class BookGenre(models.Model):
-    Genre = models.CharField(max_length=50, primary_key=True, unique=True)
+    def __str__(self):
+        """String for representing the Model object."""
+        return '{0}, {1}'.format(self.last_name, self.first_name)
