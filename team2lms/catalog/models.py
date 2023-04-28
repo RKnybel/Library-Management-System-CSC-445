d452c8fb92b5d1844ed2ccc1fbae81efc8c911be
Lauren Cappiello <103142031+Lcapp22928@users.noreply.github.com>
2023-04-28 00:20:41 -0400

    Fixed null Language Error
    When creating book on Django Admin Site previously gave null language error and did not allow for admin to complete book creation. Lang is not needed field so I removed it

--- team2lms/catalog/models.py
+++ team2lms/catalog/models.py
@@ -17,15 +17,6 @@ class Genre(models.Model):
         return self.name
 
 
-class Language(models.Model):
-    """Model representing a Language (e.g. English, French, Japanese, etc.)"""
-    name = models.CharField(max_length=200,
-                            help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
-
-    def __str__(self):
-        """String for representing the Model object (in Admin site etc.)"""
-        return self.name
-
 
 class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
@@ -41,7 +32,6 @@ class Book(models.Model):
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
     # ManyToManyField used because a genre can contain many books and a Book can cover many genres.
     # Genre class has already been defined so we can specify the object above.
-    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
     
     class Meta:
         ordering = ['title', 'author']
