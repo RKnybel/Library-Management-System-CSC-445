96ca90c0dfdbd119c8f633795ce4d0496d4879b3
Riley Knybel <spsknybeln@gmail.com>
2023-05-01 14:58:11 -0400

    Fix author and book views
    Got broke by zach's commit

--- team2lms/catalog/urls.py
+++ team2lms/catalog/urls.py
@@ -4,7 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail')
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail')
 ]
 
 
